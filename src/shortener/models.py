189384d496dd8a8f13ed2d441a4855dd8f384975
sjung <sjung2393@gmail.com>
2017-06-10 17:16:51 +0700

    Change save() method to generate a shortcode on save

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+import random
+import string
 
 # Create your models here.
+
+def generate_short_on_save(size=7, chars=string.ascii_lowercase + string.digits):
+    new_code = ''
+    for _ in range(size):
+        new_code += random.choice(chars)
+    return new_code
 
 class ShawtyURL(models.Model):
     url = models.CharField(max_length=220)
@@ -12,6 +20,13 @@ class ShawtyURL(models.Model):
     #A way to set the datetime yourself in the admin gui. Note: auto_=False
     #empty_datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
 
+
+    def save(self, *args, **kwargs):
+        #Add changes to default save() method here:
+        self.shortcode = generate_short_on_save()
+        super(ShawtyURL, self).save(*args, **kwargs)
+
+        # Modifying the default .save() method to also change the shortcode to a randomly generated one
 
     def __str__(self):
         return str(self.url)
