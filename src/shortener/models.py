f14a8cdd30b48ca55b70d30a2683f64b7a4b930d
sjung <sjung2393@gmail.com>
2017-06-14 16:09:51 +0700

    Model Manager now includes a method to refresh all the codes

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -2,8 +2,25 @@ from django.db import models
 import random
 import string
 
-from shortener.utils import generate_shortcode, check_unique_code
+from shortener.utils import create_unique_code
 # Create your models here.
+
+class ShawtyURLManager(models.Manager):
+    # To use this model manager, it must be linked to the model. Set the 'objects' field in ShawtyURL for use through queries
+    def all(self, *args, **kwargs):
+        print("im here")
+        qs = super(ShawtyURLManager, self).all(*args, **kwargs).filter(active=True)
+        return qs
+
+    def refresh_codes(self):
+        qs = ShawtyURL.objects.filter(id__gte=1)
+        new_code_count = 0
+        for q in qs:
+            q.shortcode = create_unique_code(q)
+            q.save()
+            print(q.shortcode)
+            new_code_count += 1
+        return "New codes: {i}".format(i=new_code_count)
 
 
 class ShawtyURL(models.Model):
@@ -12,18 +29,23 @@ class ShawtyURL(models.Model):
     #shortcode = models.CharField(max_length=20, null=True) null=True: Empty in db is OK
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
 
     #A way to set the datetime yourself in the admin gui. Note: auto_=False
     #empty_datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
+
+    objects = ShawtyURLManager()
 
 
     def save(self, *args, **kwargs):
         #Add changes to default save() method here:
         if self.shortcode is None or self.shortcode == '':
-            self.shortcode = check_unique_code(self)
+            self.shortcode = create_unique_code(self)
         super(ShawtyURL, self).save(*args, **kwargs)
 
         # Modifying the default .save() method to also change the shortcode to a randomly generated one
 
     def __str__(self):
         return str(self.url)
+
+
--- src/shortener/utils.py
+++ src/shortener/utils.py
@@ -4,16 +4,16 @@ import string, random
 # Instead, retrieve class name using instance.__class__
 
 
-def generate_shortcode(size=7, chars=string.ascii_lowercase + string.digits):
+def generate_code(size=7, chars=string.ascii_lowercase + string.digits):
     new_code = ''
     for _ in range(size):
         new_code += random.choice(chars)
     return new_code
 
-def check_unique_code(instance, size=7):
-    new_code = generate_shortcode(size=size)
+def create_unique_code(instance, size=7):
+    new_code = generate_code(size=size)
     Klass = instance.__class__
     qs_exists = Klass.objects.filter(shortcode=new_code).exists()
     if qs_exists:
-        return check_unique_code(size=size)
+        return create_unique_code(size=size)
     return new_code
