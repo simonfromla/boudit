5688d80fed25f5964d08ce67ab38478d563cf70b
sjung <sjung2393@gmail.com>
2017-06-22 19:57:25 +0700

    Bootstrap added

--- src/shortener/forms.py
+++ src/shortener/forms.py
@@ -2,5 +2,13 @@ from django import forms
 from .validators import validate_url, validate_dot_com
 
 class URLForm(forms.Form):
-    url = forms.CharField(max_length=200, label='Submit URL', validators=[validate_url, validate_dot_com])
+    url = forms.CharField(label='',
+        validators=[validate_url,],
+        widget= forms.TextInput(
+                attrs={
+                "placeholder": "Your very long url here . . .",
+                "class": "form-control"
+                }
+                )
+        )
 
